addabf3446118159215b520013064d4eea762cca
abramik <56174263+abramik@users.noreply.github.com>
2019-12-08 23:42:02 +0300

    Create config1.py

---
+++ homework05/config1.py
@@ -0,0 +1,6 @@
+access_token = 'b5cc1ddcff923cc196240dd30620e893e819cfd43687a96b95705ea581f9c830e0497c8a662dd0fff4151'
+CONNECTED = 200
+domain = "https://api.vk.com/method"
+user_id = 100517224
+fields = 'sex'
+v = '5.103'
